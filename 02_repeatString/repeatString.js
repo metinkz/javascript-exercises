9c0016e59c4aa81ab6fc512cbbc9e814699c9f30
Metin Karagöz <metinkzv@gmail.com>
2023-10-07 15:34:16 +0300

    do exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (greetingMessage, timesRepeated) {
+  if (timesRepeated === -1) {
+    return "ERROR";
+  }
+  string = "";
+  for (let i = 0; i < timesRepeated; i++) {
+    string += greetingMessage;
+  }
+  return string;
 };
+const number = Math.floor(Math.random() * 1000);
 
+console.log(repeatString("", number));
 // Do not edit below this line
 module.exports = repeatString;
