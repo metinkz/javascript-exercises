aa35c54b3dead27fc110a2db0bfb117f352ed56a
Metin Karagöz <metinkzv@gmail.com>
2023-10-07 15:50:25 +0300

    reverse string by the given rules

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function (normalString) {
+  let word = "";
 
+  for (let i = normalString.length - 1; i >= 0; i--) {
+    word += normalString[i];
+  }
+  return word;
 };
-
+reverseString("");
 // Do not edit below this line
 module.exports = reverseString;
