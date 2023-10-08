f0178c24eceb3e405331322dc37c47db9edd66a3
Metin Karagöz <metinkzv@gmail.com>
2023-10-08 17:31:20 +0300

    code 2 functions that converts temperatures

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempFahrenheit) {
+  const tempCelcius = (tempFahrenheit - 32) / 1.8;
+  const roundedTempCelcius = Math.round(tempCelcius * 10) / 10;
+  return roundedTempCelcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempCelcius) {
+  const tempFahrenheit = tempCelcius * 1.8 + 32;
+  const roundedTempFahrenheit = Math.round(tempFahrenheit * 10) / 10;
+  return roundedTempFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
