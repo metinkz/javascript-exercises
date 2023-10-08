0d04b96a5ad37720a26b83caeacb13f6dcdce69f
Metin Karagöz <metinkzv@gmail.com>
2023-10-08 16:25:33 +0300

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
+// My code:
+const sumAll = function (startingNumber, endingNumber) {
+  let sum = 0;
+  if (typeof startingNumber != "number" || typeof endingNumber != "number") {
+    return "ERROR";
+  }
+  if (startingNumber < 0 || endingNumber < 0) {
+    return "ERROR";
+  }
+  if (startingNumber < endingNumber) {
+    for (let i = startingNumber; i <= endingNumber; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+  if (startingNumber > endingNumber) {
+    for (let i = startingNumber; i >= endingNumber; i--) {
+      sum += i;
+    }
+    return sum;
+  }
 };
+sumAll(4, 1);
+// Solution:
 
 // Do not edit below this line
 module.exports = sumAll;
