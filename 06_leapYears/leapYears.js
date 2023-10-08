10b4381d044b47e3f63afc18edf38862c1d41c9f
Metin Karagöz <metinkzv@gmail.com>
2023-10-08 17:10:18 +0300

    make exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function (isLeapYear) {
+  if (isLeapYear % 100 === 0 && isLeapYear % 400 != 0) {
+    return false;
+  } else if (
+    isLeapYear % 100 === 0 ||
+    isLeapYear % 400 === 0 ||
+    isLeapYear % 4 === 0
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 };
-
+leapYears(1996);
 // Do not edit below this line
 module.exports = leapYears;
